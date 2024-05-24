aa61549a3bc3241fb30576e36f5ab97c80806099
Hil <hilrpatel5678@gmail.com>
2024-05-24 18:26:42 +0530

    optimize code for responsive output

--- src/component/NavOption.jsx
+++ src/component/NavOption.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavOption = () => {
-    
+    const showOption=()=>{
+        document.getElementById("navbar-solid-bg").classList.toggle("hidden");
+        document.getElementById("navbar-solid-bg").classList.toggle("block");
+    }
   return (
     <>
       <button
         data-collapse-toggle="navbar-solid-bg"
         type="button"
+        onClick={showOption}
         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         aria-controls="navbar-solid-bg"
         aria-expanded="false"
